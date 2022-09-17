d9b77ad063f301ff1f1d535d43f65de6af4767e9
diaz0107 <bdiaz0107@outlook.com>
2022-09-17 00:58:14 -0400

    changed script

--- scripts/currentyear.js
+++ scripts/currentyear.js
@@ -1,5 +1,5 @@
 function year() {
     let d = new Date();
     let b = d.getFullYear();
-    document.getElementById("currentyear").innerHTML = b;
+    document.querySelector("#currentyear").innerHTML = b;
 }
