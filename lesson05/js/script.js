42de9be93030a3f3d370938057d724da63b0f1eb
diaz0107 <bdiaz0107@outlook.com>
2022-10-15 16:16:25 -0400

    script

--- lesson05/js/script.js
+++ lesson05/js/script.js
@@ -29,3 +29,16 @@ function toggleMenu() {
 const x = document.getElementById('hamburguerBtn');
 
 x.onclick = toggleMenu;
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '12f6046a72msh59ae55138319343p19cf09jsn97e6bcbec7ef',
+		'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+	}
+};
+
+fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=sunnyvale&format=json&u=f', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
