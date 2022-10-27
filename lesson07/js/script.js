c6793334ac7505c19773de86b8f755e18d7c6f7d
diaz0107 <bdiaz0107@outlook.com>
2022-10-26 21:09:14 -0400

    small.css

--- lesson07/js/script.js
+++ lesson07/js/script.js
@@ -23,21 +23,5 @@ new Date(Date.now()).toLocaleString('en-UK',opciones2);
 
 
 
-function toggleMenu() {
-    document.getElementById('primaryNav').classList.toggle('open');
-    document.getElementById('hamburguerBtn').classList.toggle('open');
-}
 
-const x = document.getElementById('hamburguerBtn');
-
-x.onclick = toggleMenu;
-
-article = document.querySelector('#message');
-
-weekday = date.getDay();
-
-if (weekday === 1 || weekday === 2){
-    article.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-    article.setAttribute('class', 'banner-message');
-}
 
