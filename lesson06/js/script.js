55b4b3cefd6341a0f58ae462d4a5f002a61645d6
diaz0107 <bdiaz0107@outlook.com>
2022-10-27 19:09:59 -0400

    windshill

--- lesson06/js/script.js
+++ lesson06/js/script.js
@@ -1,10 +1,11 @@
 //Script for Windshill
 const temp = parseFloat(document.querySelector("#temp").textContent);
-const sspeed = parseFloat(document.querySelector("#speed").textContent);
+const speed = parseFloat(document.querySelector("#speed").textContent);
 const f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
 if (temp <= 50 && speed > 3) {
 windchill= f.toFixed(1);
-document.getElementById("windchill").innerHTML = windchill
+document.getElementById("windchill").innerHTML = windchill;
+}
 
 
 set_weather_data();
