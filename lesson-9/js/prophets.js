60bf5e1c49cd2d4c4d27be4679bb397e972e7df3
diaz0107 <bdiaz0107@outlook.com>
2022-11-14 21:29:33 -0500

    prophets.html

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -1,44 +1,51 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-
+const cards = document.querySelector('.cards');
 
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        //console.table(jsonObject);  // temporary checking for valid response and data parsing
-        const prophets = jsonObject['prophets'];
-        for (let i = 0; i < prophets.length; i++) { //for loop
-            //these are the variables needed
-            let card = document.createElement('section'); //this was the example
-            let h2 = document.createElement('h2'); //this was the example
-            // I created these ones:
-            let p = document.createElement('p'); //paragraphs 
-            let image = document.createElement('img'); //images
-            let birthDate = document.createElement('date'); //birth date
-            let birthPlace = document.createElement('place'); //birth place
-           
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
+  });
 
 
-            // this is how to fill each one of the elements, this is what it will show on top on my image on the page.
-            // It also shows the order in which everything appears.
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname; //this is the example text
-            birthDate.textContent = "Date of Birth: " + prophets[i].birthdate; // I created the following ones
-            birthPlace.textContent = "Place of Birth: " + prophets[i].birthplace;
-            
-            
-            //this is how to append the element to the card and paragraph
-            card.appendChild(h2);
-            card.appendChild(p);
-            p.appendChild(birthDate);//here these ones are connected to the paragraphs instead than to the card. 
-            p.appendChild(birthPlace);
-            card.appendChild(image);
+  function displayProphets(prophet) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let portrait = document.createElement('img');
 
-            //images
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name + prophets[i].lastname + ' ' + prophets[i].order); 
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
 
-            document.querySelector('div.cards').appendChild(card);
-        }
+    // Change the textContent property of the p elements to contain the prophet's birth date and birth place
+    p1.textContent = `Birth Date: ${prophet.birthdate}`;
+    p2.textContent = `Birth Place: ${prophet.birthplace}`;
 
-    });
+    // Build the image attributes by using the setAttribute method for the src, alt, and 
+    //loading attribute values. (Fill in the blank with the appropriate variable).
+    let sufix = "th";
+    switch (prophet.order){
+        case 1: sufix = 'st'; break;
+        case 2: sufix = 'nd'; break;
+        case 3: sufix = 'rd'; break;
+    }
+
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${sufix} Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(portrait);
+
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+  }
