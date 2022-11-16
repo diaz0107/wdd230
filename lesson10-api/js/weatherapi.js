2d9780126c95edc20c5ef0cfc3ea47b755d13ecb
diaz0107 <bdiaz0107@outlook.com>
2022-11-16 08:28:00 -0500

    weatherapi.js

--- lesson10-api/js/weatherapi.js
+++ lesson10-api/js/weatherapi.js
@@ -22,15 +22,21 @@ const url = "https://api.openweathermap.org/data/2.5/weather?id=6094817&units=im
     }
   }
     apiFetch();
+    /* Capitalize */
+    function capitalize(string) {
+      return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+    }
 
+      /* Display Results */
     function  displayResults(weatherData) {
         const currentTemp = document.querySelector('#current-temp');
         const weatherIcon = document.querySelector('#weather-icon');
-        const captionDesc = document.querySelector('figcaption');
+        const Description = document.querySelector('#currently');
         currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
       
         const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-        const desc = weatherData.weather[0].description;
+                const desc = weatherData.weather[0].description;
+                iconsrc.alt =`Icon of the current weather condition at Ottawa which is ${desc}`;
       
         weatherIcon.setAttribute('src', iconsrc);
         weatherIcon.setAttribute('alt', desc);
