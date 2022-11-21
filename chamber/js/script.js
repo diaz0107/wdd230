ccf67c2c14f97eccd61b023f37332280565be0e3
diaz0107 <bdiaz0107@outlook.com>
2022-11-21 12:15:04 -0500

    index.html

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,4 +1,5 @@
-document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
+date = new Date(Date.now());
+document.querySelector('#©').textContent = date.getFullYear();
 
 const opciones = 
 { weekday: 'long', 
@@ -17,9 +18,7 @@ day: 'numeric' };
 
 
 document.querySelector('#datetime').textContent = 
-new Date(Date.now()).toLocaleString('en-US',opciones2);
-
-
+new Date(Date.now()).toLocaleString('en-UK',opciones2);
 
 function toggleMenu() {
     document.getElementById('primaryNav').classList.toggle('open');
@@ -29,3 +28,18 @@ function toggleMenu() {
 const x = document.getElementById('hamburguerBtn');
 
 x.onclick = toggleMenu;
+
+article = document.querySelector('#message');
+
+weekday = date.getDay();
+
+/* Banner */
+
+if (weekday === 1 || weekday === 2){
+    article.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    article.setAttribute('class', 'banner-message');
+}
+
+let joint=document.querySelector('.join-us');
+joint.addEventListener("click",()=>{location.assign("https://diaz0107.github.io/wdd230/lesson08/joint.html");});
+
