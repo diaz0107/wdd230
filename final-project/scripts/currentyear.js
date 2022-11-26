71d6b8a186b8a9d1032ccf5f86c377b7b5ea1097
diaz0107 <bdiaz0107@outlook.com>
2022-11-26 13:52:37 -0500

    index.html

--- final-project/scripts/currentyear.js
+++ final-project/scripts/currentyear.js
@@ -1,5 +1,7 @@
-function year() {
-    let d = new Date();
-    let b = d.getFullYear();
-    document.querySelector("#currentyear").innerHTML = b;
-}
+// wait for the page to load then make changes
+document.addEventListener('DOMContentLoaded', function() {
+    // create a var for the current year
+    let fYear = new Date().getFullYear();
+    // add current year to the html page
+    document.querySelector('#currentYear').textContent = fYear;
+ }, false); 
--- final-project/scripts/lastupdated.js
+++ final-project/scripts/lastupdated.js
@@ -1,4 +1,5 @@
-function lastUpdated() {
-    let a = document.lastModified;
-    document.getElementById("lastmodified").innerHTML = a;
-}
+// wait for the page to load then make changes
+document.addEventListener('DOMContentLoaded', function() {
+    // update the last modified date and time
+    document.querySelector('#lastUpdated').textContent = 'Last Modified ' + document.lastModified;   
+ }, false); 
