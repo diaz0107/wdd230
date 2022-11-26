e578015ba199b3260587dd00c3b913fcd2fa8088
diaz0107 <bdiaz0107@outlook.com>
2022-11-26 14:20:54 -0500

    index.html

--- final-project/scripts/dates.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("year").innerHTML = new Date().getFullYear()
-document.getElementById("lst-updt").innerHTML = "Last updated: "+document.lastModified
--- final-project/scripts/lastupdated.js
+++
@@ -1,5 +0,0 @@
-// wait for the page to load then make changes
-document.addEventListener('DOMContentLoaded', function() {
-    // update the last modified date and time
-    document.querySelector('#lastUpdated').textContent = 'Last Modified ' + document.lastModified;   
- }, false); 
