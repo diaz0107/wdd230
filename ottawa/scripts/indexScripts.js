4fee6a2c079e0b6a990f0c17c6f0d131a3e7bf8f
Ben Diaz <bdiaz0107@outlook.com>
2022-12-14 17:42:22 -0500

    indesScripts.js

--- ottawa/scripts/indexScripts.js
+++ ottawa/scripts/indexScripts.js
@@ -11,7 +11,7 @@ function displayDrinks() {
         totalDrinks = 0;
     }
     dis.innerHTML = `<h3>Total Special Drinks Created : ${totalDrinks}</h3> <br>
-    Want a special drink just for you <a href="/fresh.html">click here</a>....
+    Want a special drink just for you <a href="./fresh.html">click here</a>....
     `;
 
 }
